456bf3740fd11d4466034cc31455666e8657e7b6
JuaniLlaberia <juanillaberia2002@gmail.com>
2026-04-09 22:52:49 -0300

    Implement api routes structure

---
+++ src/routes/analysis.py
@@ -0,0 +1,53 @@
+import json, uuid
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse, StreamingResponse
+from langgraph.types import Command
+
+from .models.request import AnalysisRequest, ResumeRequest
+from src.workflows.orquestrator.orquestrator import Orquestrator
+
+router = APIRouter(prefix="/analyze", tags=["analysis"])
+orquestrator = Orquestrator()
+
+@router.post("/")
+async def analyze_article(request: AnalysisRequest):
+    """
+    Endpoint to init claims extraction and analysis. It gets interrupted
+    for user claims re-ranking.
+    """
+    if len(request.text) >= 10_000:
+        return JSONResponse(content={"error": "Text is too long. Must be under 10,000 characters."}, status_code=400)
+
+    thread_id = str(uuid.uuid4())
+    config = {"configurable": {"thread_id": thread_id}}
+
+    async def stream():
+        async for event in orquestrator.graph.astream_events({"text": request.text}, config=config, version="v2"):
+            if event["event"] == "on_custom_event":
+                match event["name"]:
+                    case "":
+                        yield f"data: {json.dumps(event['data'])}\n\n"
+
+        state = orquestrator.graph.get_state(config)
+        yield f"data: {json.dumps({'interrupt': True, 'thread_id': thread_id, 'claims': state.values['claims']})}\n\n"
+
+    return StreamingResponse(stream(), media_type="text/event-stream")
+
+@router.post("/resume")
+async def resume_analysis(request: ResumeRequest):
+    """
+    Endpoint to resume analysis after claims re-ranking.
+    """
+    config = {"configurable": {"thread_id": request.thread_id}}
+
+    async def stream():
+        async for event in orquestrator.graph.astream_events(Command(resume=request.claims), config=config, version="v2"):
+            if event["event"] == "on_custom_event":
+                match event["name"]:
+                    case "":
+                        yield f"data: {json.dumps(event['data'])}\n\n"
+
+        state = orquestrator.graph.get_state(config)
+        yield f"data: {json.dumps({'done': True})}\n\n"
+
+    return StreamingResponse(stream(), media_type="text/event-stream")
