306f09b91ab671edfd18890bc14cf9733e182963
JuaniLlaberia <juanillaberia2002@gmail.com>
2026-04-10 09:07:34 -0300

    Create claims pydantic model

---
+++ src/workflows/orquestrator/models/claim.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from enum import Enum
+
+class ClaimLabel(Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
+class Claim(BaseModel):
+    text: str = Field(..., description="Extracted verifiable sentence")
+    relevance_score: float | None = Field(..., ge=0, le=1, description="Score from 0 to 1 based con how relevant is this claim")
+    label: ClaimLabel | None = Field(..., description="Type of claim after analysis")
+    analysis: str | None = Field(..., description="Claim analysis based on retrieve information from GFCA or RAG")
